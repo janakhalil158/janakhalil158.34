1ad54dffd9c1737b282aaf319712e142e5676558
janakhalil158 <168421945+janakhalil158@users.noreply.github.com>
2024-04-29 13:01:45 +0300

    jana initilaization

---
+++ k2.js
@@ -0,0 +1,6 @@
+document.getElementById("helpingform").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const phonenumber = document.getElementById("phone number").value;
+    const email = document.getElementById("email").value;
+     window.location.replace("anotherpage2.html");
+});
